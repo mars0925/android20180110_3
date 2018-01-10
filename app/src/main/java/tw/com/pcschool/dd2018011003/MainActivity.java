2a2cc416cbeb309b9f046a65e8841eb5342b073a
MarsChang <mars09250148@yahoo.com.tw>
2018-01-10 13:46:34 +0800

    message second version

--- app/src/main/java/tw/com/pcschool/dd2018011003/MainActivity.java
+++ app/src/main/java/tw/com/pcschool/dd2018011003/MainActivity.java
@@ -14,16 +14,32 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    //實作一個handler
     Handler handler = new Handler(){
         @Override
+        //override handler message 方法
         public void handleMessage(Message msg) {
             super.handleMessage(msg);
             Log.d("MSG", "取得一個 Message");
+            Bundle b = msg.getData();
+            String str = b.getString("data");
+
+            //在log寫入一個message測試 收到的時候會在log寫"取得一個 Message"
+            Log.d("MSG", "data:" + str);
         }
     };
     public void click1(View v)
     {
+        //實作message
         Message msg = new Message();
-        handler.sendMessage(msg);
+        //Bundle放入資料
+        Bundle b = new Bundle();
+        b.putString("data","mydata");
+        //
+        msg.setData(b);
+        //等三秒再傳
+        handler.sendMessageDelayed(msg,3000);
+        //傳出message
+        //handler.sendMessage(msg);
     }
 }
